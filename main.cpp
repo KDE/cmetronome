ec52f098a6d813126cf6aa30a6aa3a79777c9b5b
Laszlo Papp <lpapp@kde.org>
2015-01-01 12:20:05 +0000

    Remove the unnecessary __FILE__ usages

--- main.cpp
+++ main.cpp
@@ -30,31 +30,29 @@ class PulseAudio
 {
     public:
         PulseAudio() {
-            int error;
             if (!(m_pasimple = pa_simple_new(nullptr, R"("cmetronome")", PA_STREAM_PLAYBACK, nullptr,
-                            R"("cmetronome playback")", &m_pasamplespec, nullptr, nullptr, &error)))
-                throw PulseAudioException(string(__FILE__ R"(": pa_simple_new() failed: ")") + pa_strerror(error));
+                            R"("cmetronome playback")", &m_pasamplespec, nullptr, nullptr, &m_error)))
+                throw PulseAudioException(string(R"(": pa_simple_new() failed: ")") + pa_strerror(m_error));
         }
         ~PulseAudio() {
             if (m_pasimple)
                 pa_simple_free(m_pasimple);
         }
         void play() {
-            int error;
             while (true) {
                 std::basic_ifstream<uint8_t> ifs(sm_filename, ifstream::binary);
                 if(!ifs)
-                    throw PulseAudioException(string(__FILE__ R"("Please check the file's existance: ")") + sm_filename);
+                    throw PulseAudioException(string(R"("Please check the file's existance: ")") + sm_filename);
                 array<uint8_t, 64> buf;
                 ifs.read(buf.data(), buf.size());
                 if (ifs.eof()) break;
                 else if (!ifs.good())
-                    throw PulseAudioException(string(__FILE__ R"(": read() failed: ")") + strerror(errno));
-                if (pa_simple_write(m_pasimple, buf.data(), ifs.gcount(), &error) < 0)
-                    throw PulseAudioException(string(__FILE__ R"(": pa_simple_write() failed: ")") + pa_strerror(error));
+                    throw PulseAudioException(string(R"(": read() failed: ")") + strerror(errno));
+                if (pa_simple_write(m_pasimple, buf.data(), ifs.gcount(), &m_error) < 0)
+                    throw PulseAudioException(string(R"(": pa_simple_write() failed: ")") + pa_strerror(m_error));
             }
-            if (pa_simple_drain(m_pasimple, &error) < 0)
-                throw PulseAudioException(string(__FILE__ R"(": pa_simple_drain() failed: ")") + pa_strerror(error));
+            if (pa_simple_drain(m_pasimple, &m_error) < 0)
+                throw PulseAudioException(string(R"(": pa_simple_drain() failed: ")") + pa_strerror(m_error));
         }
     private:
         const pa_sample_spec m_pasamplespec = {
@@ -64,6 +62,7 @@ class PulseAudio
         };
         pa_simple *m_pasimple{nullptr};
         static constexpr const char *sm_filename = "metronome1.wav";
+        int m_error;
 };
 
 int main(int argc, char **argv)

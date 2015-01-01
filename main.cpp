4e9017c252e25204e5598bff069262e954c4e269
Laszlo Papp <lpapp@kde.org>
2015-01-01 15:55:08 +0000

    Rename the class and add -t and -s options

--- main.cpp
+++ main.cpp
@@ -9,55 +9,57 @@
 #include <vector>
 #include <cstring>
 
+#include <unistd.h>
+
 using namespace std;
 
-class PulseAudioException : public exception
+class MetronomeException : public exception
 {
     public:
-        PulseAudioException(string msg = R"(PulseAudio exception happened!)") : message(msg) {}
-        ~PulseAudioException() throw() {}
+        MetronomeException(string msg = R"(Metronome exception happened!)") : message(msg) {}
+        ~MetronomeException() throw() {}
         const char* what() const throw() { return message.c_str(); }
 
     private:
         string message;
 };
 
-class PulseAudio
+class Metronome
 {
     public:
-        PulseAudio() {
+        Metronome(uint8_t bpm, uint8_t signature) : m_bpm(bpm), m_signature(signature) {
             if (!(m_pasimple = pa_simple_new(nullptr, R"(cmetronome)", PA_STREAM_PLAYBACK, nullptr,
                             R"(cmetronome playback)", &m_pasamplespec, nullptr, nullptr, &m_error)))
-                throw PulseAudioException(string(R"(pa_simple_new() failed: )") + pa_strerror(m_error));
+                throw MetronomeException(string(R"(pa_simple_new() failed: )") + pa_strerror(m_error));
         }
-        ~PulseAudio() {
+        ~Metronome() {
             if (m_pasimple)
                 pa_simple_free(m_pasimple);
         }
         void play() {
             std::ifstream ifs(sm_filename, ifstream::binary);
             if(!ifs)
-                throw PulseAudioException(string(R"(Please check the file's existance: )") + sm_filename);
+                throw MetronomeException(string(R"(Please check the file's existance: )") + sm_filename);
             array<char, 64> buf;
             vector<char> sample;
             while (true) {
                 ifs.read(buf.data(), buf.size());
                 if (ifs.eof()) break;
                 else if (!ifs.good())
-                    throw PulseAudioException(string(R"(read() failed: )") + strerror(errno));
+                    throw MetronomeException(string(R"(read() failed: )") + strerror(errno));
                 sample.insert(sample.end(), buf.begin(), buf.begin()+ifs.gcount());
             }
             ifs.close();
             while (true) {
                 if (pa_simple_write(m_pasimple, sample.data(), sample.size(), &m_error) < 0)
-                    throw PulseAudioException(string(R"(pa_simple_write() failed: )") + pa_strerror(m_error));
+                    throw MetronomeException(string(R"(pa_simple_write() failed: )") + pa_strerror(m_error));
                 for (int i = 0; i < m_pasamplespec.rate-1; ++i) {
                     if (pa_simple_write(m_pasimple, empty_buf.data(), empty_buf.size(), &m_error) < 0)
-                        throw PulseAudioException(string(R"(pa_simple_write() failed (empty): )") + pa_strerror(m_error));
+                        throw MetronomeException(string(R"(pa_simple_write() failed (empty): )") + pa_strerror(m_error));
                 }
             }
             if (pa_simple_drain(m_pasimple, &m_error) < 0)
-                throw PulseAudioException(string(R"(pa_simple_drain() failed: )") + pa_strerror(m_error));
+                throw MetronomeException(string(R"(pa_simple_drain() failed: )") + pa_strerror(m_error));
         }
     private:
         const pa_sample_spec m_pasamplespec = {
@@ -69,14 +71,31 @@ class PulseAudio
         static constexpr const char *sm_filename = "metronome1.wav";
         int m_error;
         array<char, 4> empty_buf{{0, 0, 0, 0}};
+        uint8_t m_bpm;
+        uint8_t m_signature;
 };
 
 int main(int argc, char **argv)
 {
+    int opt, signature, bpm = 60;
+    while ((opt = getopt(argc, argv, "t:s:")) != -1) {
+        switch (opt) {
+        case 't':
+            signature = stoi(optarg);
+            break;
+        case 's':
+            bpm = stoi(optarg);
+            break;
+        default:
+            cerr << "Usage: cmetronome [-t bpm] [-s signature]\n";
+            return 1;
+        }
+    }
+
     try {
-        PulseAudio pulseAudio;
-        pulseAudio.play();
-    } catch (PulseAudioException &e) {
+        Metronome metronome(bpm, signature);
+        metronome.play();
+    } catch (MetronomeException &e) {
         cout << e.what() << endl;
     }
 }

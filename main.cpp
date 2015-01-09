dcee77acb71baf8c16c232aeaa2d47b38f1c19eb
Laszlo Papp <lpapp@kde.org>
2015-01-09 08:43:31 +0000

    Add -f support for specifying a file

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ class MetronomeException final : public exception
 class Metronome
 {
     public:
-        Metronome(uint8_t bpm, uint8_t signature) : m_bpm(bpm), m_signature(signature) {
+        Metronome(uint8_t bpm, uint8_t signature, string filepath = string()) : m_bpm(bpm), m_signature(signature), m_filepath(filepath) {
             if (!(m_pasimple = pa_simple_new(nullptr, R"(cmetronome)", PA_STREAM_PLAYBACK, nullptr,
                             R"(cmetronome playback)", &m_pasamplespec, nullptr, nullptr, &m_error)))
                 throw MetronomeException(string(R"(pa_simple_new() failed: )") + pa_strerror(m_error));
@@ -37,18 +37,20 @@ class Metronome
                 pa_simple_free(m_pasimple);
         }
         void play() {
-            ifstream ifs(sm_filename, ifstream::binary);
-            if (!ifs) throw MetronomeException(string(R"(Please check the file's existance: )") + sm_filename);
-            array<char, 64> buf;
-            vector<char> sample;
-            sample.resize(m_pasamplespec.rate*60/m_bpm*2);
-            while (ifs.read(buf.data(), buf.size()) || ifs.gcount())
-                sample.insert(sample.end(), buf.begin(), buf.begin()+ifs.gcount());
-            if (ifs.bad()) throw MetronomeException(string(R"(read() failed: )") + strerror(errno));
-            ifs.close();
+            vector<char> vsample;
+            if (!m_filepath.empty()) {
+                ifstream ifs(m_filepath, ifstream::binary);
+                if (!ifs) throw MetronomeException(string(R"(Please check the file's existance: )") + m_filepath);
+                array<char, 64> buf;
+                vsample.resize(m_pasamplespec.rate*60/m_bpm*2);
+                while (ifs.read(buf.data(), buf.size()) || ifs.gcount())
+                    vsample.insert(vsample.end(), buf.begin(), buf.begin()+ifs.gcount());
+                if (ifs.bad()) throw MetronomeException(string(R"(read() failed: )") + strerror(errno));
+                ifs.close();
+            }
             auto empty_sample_rate = (m_pasamplespec.rate-1)*60/m_bpm;
             while (true) {
-                if (pa_simple_write(m_pasimple, sample.data(), sample.size(), &m_error) < 0)
+                if (pa_simple_write(m_pasimple, vsample.data(), vsample.size(), &m_error) < 0)
                     throw MetronomeException(string(R"(pa_simple_write() failed: )") + pa_strerror(m_error));
             }
             if (pa_simple_drain(m_pasimple, &m_error) < 0)
@@ -61,23 +63,25 @@ class Metronome
             .channels = 1
         };
         pa_simple *m_pasimple{nullptr};
-        static constexpr const char *sm_filename = "metronome1_stereo.wav";
         int m_error;
         uint8_t m_bpm;
         uint8_t m_signature;
+        string m_filepath;
 };
 
 void print_usage(int status)
 {
-    cerr << "Usage: cmetronome [-t bpm] [-s signature] [-h] [-v]\n";
+    cerr << "Usage: cmetronome [-t bpm] [-s signature] [-f filepath] [-h] [-v]\n";
     exit(status);
 }
 
 int main(int argc, char **argv)
 {
     int opt, signature = 4, bpm = 60;
-    while ((opt = getopt(argc, argv, "t:s:hv")) != -1) {
+    string filepath;
+    while ((opt = getopt(argc, argv, "t:s:f:hv")) != -1) {
         switch (opt) {
+        case 'f': filepath = optarg; break;
         case 't': bpm = stoi(optarg); break;
         case 's': signature = stoi(optarg); break;
         case 'h': print_usage(EXIT_SUCCESS); break;
@@ -87,7 +91,7 @@ int main(int argc, char **argv)
     }
 
     try {
-        Metronome metronome(bpm, signature);
+        Metronome metronome(bpm, signature, filepath);
         metronome.play();
     } catch (MetronomeException &e) {
         cout << e.what() << endl;

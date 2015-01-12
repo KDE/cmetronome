c7da3cd0294ecad2c2d7413e958677db282a7448
Laszlo Papp <lpapp@kde.org>
2015-01-12 09:32:15 +0000

    Limit the signature range to 1-64 as the smallest unit is the 64th
    At least, I have never seen smaller units than that. In theory, it is possible
    to set 64/64 respectively, or even 63/64, so do not lock that out, although in
    practice, it may nearly never occur...

--- main.cpp
+++ main.cpp
@@ -97,7 +97,7 @@ void print_usage(int status)
 {
     cerr << R"(Usage: cmetronome [option parameter])" << endl << endl;
     cerr << "\t" << R"(-t The bpm that defines the tempo. It must be a positive number.)" << endl;
-    cerr << "\t" << R"(-s The signature that defines the beginning tick, e.g. 3 or 4.)" << endl;
+    cerr << "\t" << R"(-s The signature that defines the beginning tick, e.g. 3 or 4. The valid range is 1-64.)" << endl;
     cerr << "\t" << R"(-f The filepath of the beginning tick. The file specified should contain raw PCM S16LE 44.1 kHz stereo samples)" << endl;
     cerr << "\t" << R"(-g The filepath of the subsequent ticks. The file specified should contain raw PCM S16LE 44.1 kHz stereo samples)" << endl;
     cerr << "\t" << R"(-h Display this help.)" << endl;
@@ -114,7 +114,7 @@ int main(int argc, char **argv)
         case 'f': begin_filepath = optarg; break;
         case 'g': filepath = optarg; break;
         case 't': try {bpm = stoi(optarg); if (bpm < 1) print_usage(EXIT_FAILURE);} catch(exception &) {print_usage(EXIT_FAILURE);} break;
-        case 's': try {signature = stoi(optarg);} catch(exception &) {print_usage(EXIT_FAILURE);} break;
+        case 's': try {signature = stoi(optarg); if (signature < 1 or signature > 64) print_usage(EXIT_FAILURE);} catch(exception &) {print_usage(EXIT_FAILURE);} break;
         case 'h': print_usage(EXIT_SUCCESS); break;
         case 'v': cout << "cmetronome 0.1\n"; return 0;
         default: print_usage(EXIT_FAILURE); break;
